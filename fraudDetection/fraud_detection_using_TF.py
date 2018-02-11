29abb39a79ed902a7aa8234e460d7b8c7e3675bd
ketankokane94 <ketankokane94@gmail.com>
2018-02-11 21:18:31 +0530

    creating TF model
    using TF's estimator API

---
+++ fraudDetection/fraud_detection_using_TF.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 11 21:10:59 2018
+
+@author: ketankokane
+"""
+
+#fraud detection model on Synthetic transaction using TF's estimator API 
+
+import tensorflow as tf
+import pandas as pd
+
+
+def read_data():
+    pass
+
+
+def preprocess_data(df):
+#drop irrelevant columns 
+#normalize numerical columns
+    pass
+
+
+def create_feature_columns():
+    pass
+
+def split_data_for_train_test():
+    pass
+
+def train_model():
+    pass
+
+def save_model():
+    pass
+
+def load_model():
+    pass
+
+def evaluate_model():
+    pass
+
+
